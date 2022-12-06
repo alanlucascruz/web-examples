1df7b6ce61534b760974d672835b1d05c1951d38
Alan Cruz <alanlucascruz@gmail.com>
2022-12-06 14:05:20 -0300

    .

---
+++ 038 Strings Functions/main.js
@@ -0,0 +1,21 @@
+console.log("Hello World!".toLowerCase()); // hello world!
+
+console.log("Hello World!".toUpperCase()); // HELLO WORLD!
+
+console.log("Hello World!".length); // 12
+
+console.log("Hello World!".charAt(6)); // W
+
+console.log("Hello World!".includes("llo")); // true
+
+console.log("Hello World!".concat("!!")); // Hello World!!!
+
+console.log("Hello World!".slice(0, 2)); // He
+
+console.log("Hello World!".split(" ")); // ["Hello", "World"]
+
+console.log("Hello World!".replace("World", "Guys")); // Hello Guys!
+
+console.log("  Hello World   ".trim()); // Hello World
+
+console.log("Hello World!"[1]); // e
