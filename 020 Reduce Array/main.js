34ec127189438e85930398716cd429957256b022
Alan Cruz <alanlucascruz@gmail.com>
2023-02-23 10:49:49 -0300

    .

--- 020 Reduce Array/main.js
+++ 020 Reduce Array/main.js
@@ -1,9 +1,9 @@
 const cart = [
-  { id: 1, product: "Mouse", value: 279.9 },
-  { id: 2, product: "Notebook", value: 5799.0 },
+  { id: 1, product: "Mouse", value: 279.94 },
+  { id: 2, product: "Notebook", value: 5799.59 },
   { id: 3, product: "Licença de Software", value: 247.99 },
-  { id: 4, product: "Curso de Node.js", value: 497.0 },
-  { id: 5, product: "Curso de React Native", value: 1497.0 },
+  { id: 4, product: "Curso de Node.JS", value: 497.49 },
+  { id: 5, product: "Curso de React JS", value: 1497.39 },
 ];
 
 const total = cart.reduce(
